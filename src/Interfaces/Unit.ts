a304ddd4244b447e944ca3aee6ae83bb66941f74
Dan Deboer <daniel.s.deboer@gmail.com>
2019-01-28 16:12:21 -0500

    Add getters for private fields

--- src/Interfaces/Unit.ts
+++ src/Interfaces/Unit.ts
@@ -1,6 +1,9 @@
 import ValueObject from '@deboertool/value-object-interface'
 
 export default interface Unit extends ValueObject<string> {
+  code (): string
+  name (): string
+  symbol (): string
   isCode (code: string): boolean
   toString ()
 }
--- src/Unit.ts
+++ src/Unit.ts
@@ -1,14 +1,26 @@
 import UnitInterface from './Interfaces/Unit'
 
 export default class Unit implements UnitInterface {
-  constructor (private code: string, private name: string, private symbol: string) {}
+  constructor (private _code: string, private _name: string, private _symbol: string) {}
+
+  code (): string {
+    return this._code
+  }
+
+  name (): string {
+    return this._name
+  }
+
+  symbol (): string {
+    return this._symbol
+  }
 
   isCode (code: string): boolean {
-    return this.code === code
+    return this._code === code
   }
 
   toString (): string {
-    return this.symbol
+    return this._symbol
   }
 
   value (): string {
